99734d6b939e935b72e6e22826fd12b5977ab6d4
ardasindhuja <114642361+ardasindhuja@users.noreply.github.com>
2023-02-25 15:00:50 +0530

    Create JAVA.java

---
+++ JAVA.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class main{
+ public static void main (String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String s=sc.nextLine();
+        int l=s.length();
+        for (int i=l-1;i>=0;i--)
+        { 
+            System.out.print(s.charAt(i));
+        }
+ }
+}
+
+        
